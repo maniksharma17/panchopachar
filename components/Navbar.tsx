4a93d37b840334bf2f01f4fd051beb6b819ccfd3
Manik Sharma <maniksharma.rke@gmail.com>
2024-08-30 18:39:59 +0530

    changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,8 +9,8 @@ const Navbar = () => {
       <Image className='m-auto mt-2'
         src={'https://panchopachar.s3.ap-south-1.amazonaws.com/logo3.png'}
         alt='logo'
-        width={300}
-        height={300}
+        width={200}
+        height={200}
       ></Image>
 
       
